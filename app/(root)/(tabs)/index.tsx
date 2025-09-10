9ec6b58a5295312aa37ec2d491486a74c9ae2eb0
NeonSyntax <dddadang322@gmail.com>
2025-09-10 23:17:16 +0800

    Initial commit: add ReState project

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,20 +1,148 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import FeaturedCard, { Card } from "@/components/Cards";
+import Filters from "@/components/Filters";
+import NoResults from "@/components/NoResults";
+import Search from "@/components/Search";
+import icons from "@/constants/icons";
+import { getLatestProperties, getProperties } from "@/lib/appwrite";
+import { useGlobalContext } from "@/lib/global-provider";
+import { useAppwrite } from "@/lib/useAppwrite";
+import { router, useLocalSearchParams } from "expo-router";
+import { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+  const {user} = useGlobalContext();
+  const params = useLocalSearchParams<{query?: string; filter?: string}>();
+
+  const {data: latestProperties, loading: latestPropertiesLoading} = useAppwrite({
+    fn: getLatestProperties
+  })
+
+  const {data: properties , loading, refetch} = useAppwrite({
+    fn: getProperties,
+    params: {
+      filter: params.filter!,
+      query: params.query!,
+      limit: 6,
+    },
+    skip: true,
+  })
+
+  const handleCardPress = (id: string) => router.push(`/properties/${id}`);
+
+  useEffect(() => {
+    refetch({
+      filter: params.filter!,
+      query: params.query!,
+      limit: 6,
+    })
+  }, [params.filter, params.query])
+
+    const [time, setTime] = useState<string>("");
+
+    useEffect(() => {
+      const interval = setInterval(() => {
+        const now = new Date();
+        // Format jam: HH:MM:SS
+        const formatted = now.toLocaleTimeString("id-ID", {
+          hour: "2-digit",
+          minute: "2-digit",
+          second: "2-digit",
+        });
+        setTime(formatted);
+      }, 1000);
+
+      return () => clearInterval(interval); // bersihin interval biar gak bocor memory
+    }, []);
+    
+    function getGreeting() {
+      const hour = new Date().getHours();
+
+      if (hour >= 4 && hour < 11) return "Selamat Pagi";   // 04:00 - 10:59
+      if (hour >= 11 && hour < 15) return "Selamat Siang"; // 11:00 - 14:59
+      if (hour >= 15 && hour < 18) return "Selamat Sore";  // 15:00 - 17:59
+      return "Selamat Malam";                              // 18:00 - 03:59
+    }
+
+    const [greeting, setGreeting] = useState(getGreeting());
+
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setGreeting(getGreeting());
+      }, 60000); // update tiap 1 menit
+      return () => clearInterval(interval);
+    }, []);
+
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="font-bold my-10 font-rubik text-3xl">Welcome to ReState</Text>
-      <Link href="/sign-in">Sign In</Link>
-      <Link href="/explore">Explore</Link>
-      <Link href="/profile">Profile</Link>
-      <Link href="/properties/1">Properties</Link>
+  <SafeAreaView className="bg-white h-full">
+    <FlatList 
+      data = {properties}
+      renderItem = {({item}) => <Card item={item} onPress={() => handleCardPress(item.$id)}/>}
+      keyExtractor={(item) => item.$id}
+      numColumns={2}
+      contentContainerClassName="pb-32"
+      columnWrapperClassName="flex gap-5 px-5"
+      showsVerticalScrollIndicator={false}
+      ListEmptyComponent={
+        loading ? (
+          <ActivityIndicator size="large" className="text-primary-300 mt-5"/>
+        ) : <NoResults />
+      }
+      ListHeaderComponent={
+
+    <View className="px-5">
+
+      <View className="flex flex-row items-center justify-between mt-5">
+        <View className="flex flex-row items-center">
+          <Image source={{uri: user?.avatar}} className="size-12 rounded-full"/>
+          <View className="flex flex-col items-start ml-2 justify-center">
+            <Text className="text-xs font-rubik text-black-100">{greeting}</Text>
+            <Text className="text-base font-rubik-medium text-black-300">{user?.name}</Text>
+          </View>
+        </View>
+        <Image source={icons.bell} className="size-6"/>
+      </View>
+      
+      <Search />
+      <View className="my-5">
+        <View className="flex flex-row items-center justify-between">
+          <Text className="text-xl font-rubik-bold text-black-300">Featured</Text>
+          <TouchableOpacity>
+            <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
+          </TouchableOpacity>
+        </View>
+
+        {latestPropertiesLoading ? 
+          <ActivityIndicator size="large" className="text-primary-300" />
+         : !latestProperties || latestProperties.length === 0 ? <NoResults /> : (
+
+        <FlatList 
+        data ={latestProperties}
+        renderItem = {({item}) => <FeaturedCard item={item} onPress={() => handleCardPress(item.$id)}/>}
+        keyExtractor={(item) => item.$id}
+        horizontal
+        bounces={false}
+        showsHorizontalScrollIndicator={false}
+        contentContainerClassName="flex gap-5 mt-5"
+        />)}
+      </View>
+
+        <View className="flex flex-row items-center justify-between">
+          <Text className="text-xl font-rubik-bold text-black-300">Our Recommendation</Text>
+          <TouchableOpacity>
+            <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
+          </TouchableOpacity>
+        </View>
+
+        <Filters />
+
     </View>
+
+      }
+    />
+    
+  </SafeAreaView>
   );
 }
--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,74 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { Alert, Image, ScrollView, Text, TouchableOpacity, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+import icons from '@/constants/icons'
+import images from '@/constants/images'
+import { login } from '@/lib/appwrite'
+import { useGlobalContext } from '@/lib/global-provider'
+import { Redirect } from 'expo-router'
 
 const SignIn = () => {
+  const { refetch, loading, isLoggedIn} = useGlobalContext();
+
+  if(!loading && isLoggedIn) return <Redirect href="/" />
+
+  const handlelogin = async () => {
+    const result = await login ();
+
+    if (result) {
+      refetch();
+    } else {
+      Alert.alert('Error', 'Failed to login');
+    }
+  }
+
   return (
-    <View>
-      <Text>sign-in</Text>
-    </View>
+  <SafeAreaView className='bg-white h-full'>
+    <ScrollView 
+      contentContainerClassName='flex-grow justify-between' 
+      contentContainerStyle={{ paddingBottom: 40 }} // kasih jarak bawah
+      showsVerticalScrollIndicator={false}
+    >
+      <Image source={images.onboarding} className='w-full h-4/6' resizeMode='contain'/>
+      
+      <View className='px-10'>
+        <Text className='text-base text-center uppercase font-rubik text-black-200'>
+          Welcome to ReState
+        </Text>
+
+        <Text className='text-3xl font-rubik-bold text-black-300 text-center mt-2'>
+          Let's Get You Closer to {"\n"}
+          <Text className='text-primary-300'>Your Ideal Home</Text>
+        </Text>
+
+        <Text className='text-lg font-rubik text-black-200 text-center mt-12'>
+          Login to ReState with Google
+        </Text>
+
+        <TouchableOpacity 
+          onPress={handlelogin} 
+          className='bg-white rounded-full w-full py-4 mt-5 shadow-md shadow-zinc-300'
+          style={{
+            elevation: 3, // khusus Android
+            shadowColor: '#000', // iOS
+            shadowOffset: { width: 0, height: 2 },
+            shadowOpacity: 0.2,
+            shadowRadius: 3,
+          }}
+        >
+          <View className='flex flex-row items-center justify-center'>
+            <Image source={icons.google} className='w-5 h-5' resizeMode='contain'/>
+            <Text className='text-lg font-rubik-medium text-black-300 ml-2'>
+              Continue with Google
+            </Text>
+          </View>
+        </TouchableOpacity>
+
+      </View>
+    </ScrollView>
+  </SafeAreaView>
+
   )
 }
 
